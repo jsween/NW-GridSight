9f527137021a89b942573814e8482a2c37bb049f
Jon Sweeney <jonsweeney26@gmail.com>
2026-04-18 12:11:28 -0700

    Add project files.

---
+++ NW-GridSight/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NW_GridSight.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
